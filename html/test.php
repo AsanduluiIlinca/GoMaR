d7d74e41e32a899633caa06c8b1e0bab04732588
Dascalu Marilena <marilena.dascalu@info.uaic.ro>
2020-06-10 19:09:24 +0300

    change question array structure

--- html/test.php
+++ html/test.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     include 'databaseConnection.php';
-    // echo '<pre>'; print_r( $_SESSION); echo '</pre>';
+// echo '<pre>'; print_r( $_SESSION); echo '</pre>';
 
     $stmt = $conn->prepare("SELECT * FROM question WHERE category_id=:categoryID");
     $stmt -> bindParam(':categoryID', $_SESSION['categoryID']);
@@ -64,23 +64,22 @@
                 $stmt = $conn->prepare("SELECT * FROM answer WHERE question_id=:question_id");
                 $stmt -> bindParam(':question_id', $j);
                 $stmt -> execute();
-                $answwersArray = $stmt -> fetchAll();
-                //  echo '<pre>'; print_r( $answwersArray); echo '</pre>';
-                $questionArray[] = array('id'=>$j,'question'=>$i['question'], 'answers'=>array('0'=>$answwersArray[0]['answer'],'1'=>$answwersArray[1]['answer'],'valid_0'=>$answwersArray[0]['valid'],'valid_1'=>$answwersArray[1]['valid']));
+                $answersArray = $stmt -> fetchAll();
+                //  echo '<pre>'; print_r( $answersArray); echo '</pre>';
+                $questionArray[] = array('id'=>$j,'question'=>$i['question'],'score'=>$i['difficulty'], 'answers'=>array('0'=>array('id'=>$answersArray[0]['id'],'answer'=>$answersArray[0]['answer'],'valid'=>$answersArray[0]['valid']),
+                                                                                                                         '1'=>array('id'=>$answersArray[1]['id'],'answer'=>$answersArray[1]['answer'],'valid'=>$answersArray[1]['valid'])));
             }
         }
     }
-
-  
-    
-    // array_push($randIndex, array_rand($midleDiff,2));
-    // array_push($randIndex, array_rand($higherDiff,1));
+    // echo '<pre>'; print_r( $_POST['answer1']); echo '</pre>';
+    // echo '<pre>'; print_r( $_POST['answer2']); echo '</pre>';
+    // if(!empty($_POST['answer1']))
+    // {
+    //     echo '<pre>'; print_r( $_POST['answer1']); echo '</pre>';
+    // }
+    $_SESSION['questionAnswer']= $questionArray;
     // echo '<pre>'; print_r( $questionArray); echo '</pre>';
-    // echo '<pre>'; print_r($lowerDiff)."scazut"; echo '</pre>';
-    // echo '<pre>'; print_r($midleDiff)."medium"; echo '</pre>';
-    // echo '<pre>'; print_r($higherDiff)."ridicat"; echo '</pre>';
-
-    // echo '<pre>'; echo $size; echo '</pre>';
+   
 
 ?>
 
@@ -116,159 +115,138 @@
         <div class="central-container scrollable">
             <div class="quiz-question">
                 <p class="question">
-                    <!-- How should we behave at the restaurant? -->
                     <?php echo $questionArray[0]['question']; ?>
                 </p>
             </div>
             <div class="quiz-selector">
                 <div class="answer">
-                    <input type="checkbox" id="answer1" name="answer1">
+                    <input type="checkbox" id="answer1" name="answer1" value="$questionArray[0]['answer'][0]['valid']">
                     <span class="sentence">
-                        <!-- We should follow a certain etiquette -->
-                        <?php echo $questionArray[0]['answers'][0]; ?>
+                        <?php echo $questionArray[0]['answers'][0]['answer']; ?>
                     </span>
                 </div>
                 <div class="answer">
-                    <input type="checkbox" id="answer2" name="answer2">
+                    <input type="checkbox" id="answer2" name="answer2" value="$questionArray[0]['answer'][1]['valid']">
                     <span class="sentence">
-                        <!-- We should behave normally -->
-                        <?php echo $questionArray[0]['answers'][1]; ?>
+                        <?php echo $questionArray[0]['answers'][1]['answer']; ?>
                     </span>
                 </div>
             </div>
             <div class="quiz-question">
                 <p class="question">
-                    <!-- How should we behave at the restaurant? -->
                     <?php echo $questionArray[1]['question']; ?>
                 </p>
             </div>
             <div class="quiz-selector">
                 <div class="answer">
-                    <input type="checkbox" id="answer1" name="answer1">
+                    <input type="checkbox" id="answer1" name="answer3" value="$questionArray[1]['answer'][0]['valid']">
                     <span class="sentence">
-                        <!-- We should follow a certain etiquette -->
-                        <?php echo $questionArray[1]['answers'][0]; ?>
+                        <?php echo $questionArray[1]['answers'][0]['answer']; ?>
                     </span>
                 </div>
                 <div class="answer">
-                    <input type="checkbox" id="answer2" name="answer2">
+                    <input type="checkbox" id="answer2" name="answer4" value="$questionArray[1]['answer'][1]['valid']">
                     <span class="sentence">
-                        <!-- We should behave normally -->
-                        <?php echo $questionArray[1]['answers'][1]; ?>
+                        <?php echo $questionArray[1]['answers'][1]['answer']; ?>
                     </span>
                 </div>
             </div>
             <div class="quiz-question">
                 <p class="question">
-                    <!-- How should we behave at the restaurant? -->
                     <?php echo $questionArray[2]['question']; ?>
                 </p>
             </div>
             <div class="quiz-selector">
                 <div class="answer">
-                    <input type="checkbox" id="answer1" name="answer1">
+                    <input type="checkbox" id="answer1" name="answer5" value="$questionArray[2]['answer'][0]['valid']">
                     <span class="sentence">
-                        <!-- We should follow a certain etiquette -->
-                        <?php echo $questionArray[2]['answers'][0]; ?>
+                        <?php echo $questionArray[2]['answers'][0]['answer']; ?>
                     </span>
                 </div>
                 <div class="answer">
-                    <input type="checkbox" id="answer2" name="answer2">
+                    <input type="checkbox" id="answer2" name="answer6" value="$questionArray[2]['answer'][1]['valid']">
                     <span class="sentence">
-                        <!-- We should behave normally -->
-                        <?php echo $questionArray[2]['answers'][1]; ?>
+                        <?php echo $questionArray[2]['answers'][1]['answer']; ?>
                     </span>
                 </div>
             </div>
             <div class="quiz-question">
                 <p class="question">
                 <?php echo $questionArray[3]['question']; ?>
-                    <!-- How should we behave at the restaurant? -->
                 </p>
             </div>
             <div class="quiz-selector">
                 <div class="answer">
-                    <input type="checkbox" id="answer1" name="answer1">
+                    <input type="checkbox" id="answer1" name="answer7" value="$questionArray[3]['answer'][0]['valid']">
                     <span class="sentence">
-                        <!-- We should follow a certain etiquette -->
-                        <?php echo $questionArray[3]['answers'][0]; ?>
+                        <?php echo $questionArray[3]['answers'][0]['answer']; ?>
                     </span>
                 </div>
                 <div class="answer">
-                    <input type="checkbox" id="answer2" name="answer2">
+                    <input type="checkbox" id="answer2" name="answer8" value="$questionArray[3]['answer'][1]['valid']">
                     <span class="sentence">
-                        <!-- We should behave normally -->
-                        <?php echo $questionArray[3]['answers'][1]; ?>
+                        <?php echo $questionArray[3]['answers'][1]['answer']; ?>
                     </span>
                 </div>
             </div>
             <div class="quiz-question">
                 <p class="question">
                 <?php echo $questionArray[4]['question']; ?>
-                    <!-- How should we behave at the restaurant? -->
                 </p>
             </div>
             <div class="quiz-selector">
                 <div class="answer">
-                    <input type="checkbox" id="answer1" name="answer1">
+                    <input type="checkbox" id="answer1" name="answer9" value="$questionArray[4]['answer'][0]['valid']">
                     <span class="sentence">
-                        <!-- We should follow a certain etiquette -->
-                        <?php echo $questionArray[4]['answers'][0]; ?>
+                        <?php echo $questionArray[4]['answers'][0]['answer']; ?>
                     </span>
                 </div>
                 <div class="answer">
-                    <input type="checkbox" id="answer2" name="answer2">
+                    <input type="checkbox" id="answer2" name="answer10" value="$questionArray[4]['answer'][1]['valid']">
                     <span class="sentence">
-                        <!-- We should behave normally -->
-                        <?php echo $questionArray[4]['answers'][1]; ?>
+                        <?php echo $questionArray[4]['answers'][1]['answer']; ?>
                     </span>
                 </div>
             </div>
             <div class="quiz-question">
                 <p class="question">
-                    <!-- How should we behave at the restaurant? -->
                     <?php echo $questionArray[5]['question']; ?>
                 </p>
             </div>
             <div class="quiz-selector">
                 <div class="answer">
-                    <input type="checkbox" id="answer1" name="answer1">
+                    <input type="checkbox" id="answer1" name="answer11" value="$questionArray[5]['answer'][0]['valid']">
                     <span class="sentence">
-                        <!-- We should follow a certain etiquette -->
-                        <?php echo $questionArray[5]['answers'][0]; ?>
+                        <?php echo $questionArray[5]['answers'][0]['answer']; ?>
                     </span>
                 </div>
                 <div class="answer">
-                    <input type="checkbox" id="answer2" name="answer2">
+                    <input type="checkbox" id="answer2" name="answer12" value="$questionArray[5]['answer'][1]['valid']">
                     <span class="sentence">
-                        <!-- We should behave normally -->
-                        <?php echo $questionArray[5]['answers'][1]; ?>
+                        <?php echo $questionArray[5]['answers'][1]['answer']; ?>
                     </span>
                 </div>
             </div>
             <div class="quiz-question">
                 <p class="question">
-                    <!-- How should we behave at the restaurant? -->
                     <?php echo $questionArray[6]['question']; ?>
                 </p>
             </div>
             <div class="quiz-selector">
                 <div class="answer">
-                    <input type="checkbox" id="answer1" name="answer1">
+                    <input type="checkbox" id="answer1" name="answer13" value="$questionArray[6]['answer'][0]['valid']">
                     <span class="sentence">
-                        <!-- We should follow a certain etiquette -->
-                        <?php echo $questionArray[6]['answers'][0]; ?>
+                        <?php echo $questionArray[6]['answers'][0]['answer']; ?>
                     </span>
                 </div>
                 <div class="answer">
-                    <input type="checkbox" id="answer2" name="answer2">
+                    <input type="checkbox" id="answer2" name="answer14" value="$questionArray[6]['answer'][1]['valid']">
                     <span class="sentence">
-                        <!-- We should behave normally -->
-                        <?php echo $questionArray[6]['answers'][1]; ?>
+                        <?php echo $questionArray[6]['answers'][1]['answer']; ?>
                     </span>
                 </div>
             <div>
-                <span class="answer-btn" onclick="window.location.href = 'test.html';">Sent your answers</span>
+                <span class="answer-btn" onclick="window.location.href = 'test.php';">Sent your answers</span>
             </div>
         </div>
     </div>

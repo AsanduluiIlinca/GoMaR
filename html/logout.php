e1a97f62ab078d22f0ccab894397b51ca4e39675
Ilinca Asandului <antonia.ilinca7@yahoo.com>
2020-06-11 02:56:03 +0300

    Update logout.php

--- html/logout.php
+++ html/logout.php
@@ -1,5 +1,11 @@
 <?php
     session_start();
-    session_unset($_SESSION['user_id']);
+    session_destroy();
+    /*session_unset($_SESSION['user_id']);
+    session_unset($_SESSION['categoryID']);
+    session_unset($_SESSION['categoryName']);
+    session_unset( $_SESSION['questionAnswer']);
+    session_unset($_SESSION['selectedAnswers'] );
+    session_unset($_SESSION['username']);*/
     header("Location: home.html");
 ?>

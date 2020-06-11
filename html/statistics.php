6ceb3a1b529ac2f18c484b26aa5ae59ff4449c25
Ilinca Asandului <antonia.ilinca7@yahoo.com>
2020-06-11 13:59:44 +0300

    Update statistics

--- html/statistics.php
+++ html/statistics.php
@@ -1,45 +1,39 @@
 <?php
-    session_start();
-    include 'databaseConnection.php';
+session_start();
+include 'databaseConnection.php';
 
-    $records = $conn->prepare('SELECT score FROM user WHERE user_id =:user_id');
-    $records->bindParam(':user_id', $_SESSION['user_id']);
-    $records_exec=$records->execute();
-    $results = $records->fetchAll();
+$records = $conn->prepare('SELECT score FROM user WHERE user_id =:user_id');
+$records->bindParam(':user_id', $_SESSION['user_id']);
+$records_exec = $records->execute();
+$results = $records->fetchAll();
 
-    $records2 = $conn->prepare('SELECT score FROM category_score WHERE user_id =:user_id');
-    $records2->bindParam(':user_id', $_SESSION['user_id']);
-    $records2_exec=$records2->execute();
-    $results2 = $records2->fetchAll();
+$records2 = $conn->prepare('SELECT score FROM category_score WHERE user_id =:user_id');
+$records2->bindParam(':user_id', $_SESSION['user_id']);
+$records2_exec = $records2->execute();
+$results2 = $records2->fetchAll();
 
-    if( $results == false)
-    {
-       $total_score=0;
-    }
-    else 
-    {
-        $total_score = $results['score'];
-    }
-   
-    for($i=0;$i<sizeof($results2);$i++)
-    { 
-        $total_score = $total_score + $results2[$i][0];
-    } 
-    $user_id = $_SESSION['user_id'];
-    $sql = "UPDATE user SET score=:score WHERE id=:user_id";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute(array(':score'=>$total_score, ':user_id'=>$user_id));
-    
- 
-    if (isset($_POST["submit_1"]))
-    {
-        header("Location: leaderboard.php");
-    }
+if ($results == false) {
+    $total_score = 0;
+} else {
+    $total_score = $results['score'];
+}
 
-    if (isset($_POST["submit_2"]))
-    {
-        header("Location: myprogress.php");
-    }
+for ($i = 0; $i < sizeof($results2); $i++) {
+    $total_score = $total_score + $results2[$i][0];
+}
+$user_id = $_SESSION['user_id'];
+$sql = "UPDATE user SET score=:score WHERE id=:user_id";
+$stmt = $conn->prepare($sql);
+$stmt->execute(array(':score' => $total_score, ':user_id' => $user_id));
+
+
+if (isset($_POST["submit_1"])) {
+    header("Location: leaderboard.php");
+}
+
+if (isset($_POST["submit_2"])) {
+    header("Location: myprogress.php");
+}
 ?>
 <!doctype html>
 
@@ -49,7 +43,7 @@
     <meta charset="utf-8">
     <title>GoMaR</title>
     <link rel="stylesheet" href="../css/styles.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
 </head>
 
 <body>
@@ -70,11 +64,10 @@
     </div>
     <div class="container">
         <form class="central-container scrollable" name="form" action="statistics.php" method="POST">
-            <?php echo "Score for last  test was: ".$_SESSION['lastTest']." from a total of ".$_SESSION['totalScore'];?></br>
+            <?php echo "Score for last  test was: " . $_SESSION['lastTest'] . " from a total of " . $_SESSION['totalScore']; ?></br>
             <input class="b" type="submit" name="submit_1" onclick="window.location.href = 'leaderboard.php';" value='Leaderboard'>
             <input class="b" type="submit" name="submit_2" onclick="window.location.href = 'myprogress.php';" value='Score by category'>
-    </form>
+        </form>
     </div>
 </body>
-
 </html>

bdfd2eeaef0b8a2b02ba09fb562f4107d541a2af
Dascalu Marilena <marilena.dascalu@info.uaic.ro>
2020-06-13 19:56:09 +0300

    add information

---
+++ resources/insertInformation.sql
@@ -0,0 +1,125 @@
+INSERT INTO informations (
+    category_id,
+    link_site, 
+    path_image
+)
+VALUES
+(
+    1,
+    'https://www.merriam-webster.com/dictionary/academic',
+    '../resources/academic-meaning.png'
+),
+(
+    1,
+    'https://college.harvard.edu/admissions/why-harvard/academic-environment',
+    '../resources/harvard.png'
+
+),
+(
+    1,
+    'https://link.springer.com/article/10.1007/BF01680529',
+    '../resources/studentPerceptions.png'
+
+),
+(
+    1,
+    'https://oiss.yale.edu/life-at-yale/cultural-adjustment-transition/academic-environment',
+    '../resources/academic2.png'
+
+),
+(
+    2,
+    'https://family.lovetoknow.com/about-family-values/meaning-family',
+    '../resources/meaningOfFamily.png'
+
+),
+(
+    2,
+   ' https://en.wikipedia.org/wiki/Family',
+   '../resources/family-wiki.png.'
+
+),
+(
+    2,
+    'https://medium.com/sandbox/what-does-family-mean-for-you-and-how-has-it-shaped-you-8cbc1aaaee02',
+    '../resources/shaped.png'
+
+),
+(
+    2,
+    'https://www.travelance.ca/blog/family-mean/',
+    '../resources/what-family-meaning.png'
+
+),
+(
+    3,
+    'https://en.wikipedia.org/wiki/Social_group',
+    '../resources/group-wiki.png'
+
+),
+(
+    3,
+    'https://courses.lumenlearning.com/sociology/chapter/types-of-groups/',
+    '../resources/types-group.png'
+),
+(
+    3,
+    'https://www.macmillanthesaurus.com/topics/groups-of-people',
+    '../resources/group-of-people.png'
+
+),
+(
+    3,
+    'https://www.khanacademy.org/test-prep/mcat/society-and-culture/social-structures/a/what-are-social-groups-and-social-networks',
+    '../resources/what-are-social-groups.png'
+
+),
+(
+    4,
+    'https://en.wikipedia.org/wiki/Society',
+    '../resources/society-wiki.png'
+
+),
+(
+    4,
+    'https://www.academia.edu/20198715/Importance_of_Communication_in_Society',
+    '../resources/importance-of-communication.png'
+
+),
+(
+    4,
+    'https://thedecisionlab.com/impact-social-components-human-behaviour/',
+    '../resources/society-influence.png'
+
+),
+(
+    4,
+    'https://www.fnu.edu/communication-todays-society/',
+    '../resources/communication-society.png'
+
+),
+(
+    5,
+    'https://www.wikihow.com/Behave-at-Work',
+    '../resources/how-to-behave-at-work.png'
+
+),
+(
+    5,
+    'http://www.executivestyle.com.au/20-tips-to-dress-appropriately-for-work-2xvcy',
+    '../resources/how-to-dress.png'
+
+),
+(
+    5,
+    'https://blog.dce.harvard.edu/professional-development/how-communicate-more-effectively-workplace',
+    '../resources/communicate-work.png'
+
+),
+(
+    5,
+    'https://www.hongkiat.com/blog/positive-working-environment/',
+    '../resources/workplace-environment.png'
+
+)
+
